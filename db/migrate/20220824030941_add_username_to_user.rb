7a06c185792f030305b2068cedd12ad46b9e601c
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-24 12:43:49 +0900

    追加実装修正

--- db/migrate/20220824030941_add_username_to_user.rb
+++ db/migrate/20220824030941_add_username_to_user.rb
@@ -1,5 +1,7 @@
 class AddUsernameToUser < ActiveRecord::Migration[6.0]
+  #usernameカラムをstring型(文字列型)に指定(rails g migration AddUsernameToUser username:string)
   def change
     add_column :users, :username, :string, null: false, default: ""
+    #カラムにNOT NULL制約（null: false）を追加します。NOT NULL制約とは、カラムに格納する値としてNULLを禁止
   end
 end
