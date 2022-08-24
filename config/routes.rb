39176ca62a511eccb4a7a61b61e8e59268832a89
Kawase-Ryo <2801r.kawase@gmail.com>
2022-08-24 13:05:02 +0900

    コメント機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 
+  # ここにusersコントローラーのshowアクションのルーティングを追加する
   get '/users/:id', to: 'users#show', as: 'user'
-
+  
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
     resources :likes, only: %i(create destroy)
+  # ルーティングをネスト（入れ子）にすることで、親子関係をルーティングで表すことができる。commentsに関してはpostsに対して「子」の関係になるので、ネストすることで、どの投稿に紐づくかを明示  
+    resources :comments, only: %i(create destroy)
   end
+  #onlyを使うと、自動で生成される7つのアクションのうちどのアクションのルーティングを追加するか絞り込むことができる
+
 end
